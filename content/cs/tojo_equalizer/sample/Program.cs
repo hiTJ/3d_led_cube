607d9de1d1ba57da320503f319799aa40196eebb
東條寛之 <hiroyuki.tojo@fxat.co.jp>
2017-07-28 15:16:39 +0900

    Add the description. Correct some of function and valuables name.

--- content/cs/tojo_equalizer/sample/Program.cs
+++ content/cs/tojo_equalizer/sample/Program.cs
@@ -34,23 +34,60 @@ namespace sample
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool SetDllDirectory(string lpPathName);
 
+        /// <summary>
+        /// サンプリング周波数
+        /// </summary>
+        const int sampleRate = 44100;
 
+        /// <summary>
+        /// ステレオ(1)、モノラル(2)
+        /// </summary>
+        const int channels = 1;
+
+        /// <summary>
+        /// サンプリング点数
+        /// </summary>
         const int samplePoint = 1024;
+
+        /// <summary>
+        /// x軸座標の点数（横）
+        /// </summary>
         const int xAxisPoints = 16;
+
+        /// <summary>
+        /// y軸座標の点数（縦）
+        /// </summary>
         const int yAxisPoints = 32;
+
+        /// <summary>
+        /// z軸座標の点数（奥行き）
+        /// </summary>
         const int zAxisPoints = 8;
+
+        /// <summary>
+        /// サンプリングした点数を分割する値
+        /// </summary>
+        const int divider = 4;
+
+        /// <summary>
+        /// x軸のシフト値(大きいほど低周波をカットして高周波を増やす)
+        /// </summary>
+        const int xAxisShifter = 4;
+
+        //係数は適当
+        const int ampCoefficient = 20;
 
         static void Main(string[] args)
         {
             String test = AppDomain.CurrentDomain.BaseDirectory;
             DirectoryInfo libDir = Directory.GetParent(test).Parent.Parent.Parent.Parent;
             SetDllDirectory(Path.Combine(libDir.FullName,"00_lib"));
-
+            //SetDllDirectory("00_lib");
 
             using (var waveIn = new WaveInEvent())
             {
                 List<float> recorded = new List<float>();
-                waveIn.WaveFormat = new WaveFormat(sampleRate: 44100, channels: 1);
+                waveIn.WaveFormat = new WaveFormat(sampleRate: sampleRate, channels: channels);
 
                 waveIn.DataAvailable += (_, e) =>
                 {
@@ -67,37 +104,59 @@ namespace sample
                         {
                             var computed = ComputeFFT(recorded);
                             int value = 0;
-                            for (int i = 0; i < computed.Count / 2; i++)
+                            int pointsPerBlock = samplePoint / xAxisPoints / divider;
+                            int xAxisMax = computed.Count / divider + xAxisShifter * pointsPerBlock;
+                            for (int i = xAxisShifter * pointsPerBlock; i < xAxisMax; i++)
                             {
-                                int blockPoints = samplePoint / xAxisPoints / 2;
                                 value += (int)(computed[i]);
-                                if (i % blockPoints == 0 && i != 0)
+                                if (i % pointsPerBlock == 0 && i >= xAxisShifter * pointsPerBlock)
                                 {
                                     for (int z = 0; z < zAxisPoints; z++)
                                     {
-                                        if (value > blockPoints * 32)
+                                        if (value > pointsPerBlock * yAxisPoints)
                                         {
-                                            value = blockPoints * 32;
+                                            value = pointsPerBlock * yAxisPoints;
                                         }
-                                        int highestY = 32 - value / blockPoints;
+                                        int highestY = yAxisPoints - value / pointsPerBlock;
                                         for(int y = yAxisPoints; y > highestY; y--)
                                         {
                                             int color = 0;
-                                            if(y < 10)
+                                            if(y < 4)
                                             {
                                                 color = 0xFF0000;
                                             }
-                                            else
+                                            else if(4 <= y && y < 8)
+                                            {
+                                                color = 0xFF7700;
+                                            }
+                                            else if(8 <= y && y < 12)
+                                            {
+                                                color = 0x77FF00;
+                                            }
+                                            else if(16 <= y && y < 20)
                                             {
                                                 color = 0x00FF00;
                                             }
-                                            SetLed(i / blockPoints, y, z, color);
+                                            else if(20 <= y && y < 24)
+                                            {
+                                                color = 0x0077FF;
+                                            }
+                                            else if(24 <= y && y < 28)
+                                            {
+                                                color = 0x0000FF;
+                                            }
+                                            else
+                                            {
+                                                color = 0x000077;
+                                            }
+                                            SetLed(i / pointsPerBlock - xAxisShifter, y, z, color);
                                         }
                                     }
-                                    Show();
                                     value = 0;
                                 }
                             }
+                            Show();
+                            Wait(10);
                             Clear();
                             recorded.Clear();
                         }
@@ -111,6 +170,11 @@ namespace sample
             Console.WriteLine("Program is ended successfully.");
         }
 
+        /// <summary>
+        /// 音データにFFT処理を施し、FFT結果の絶対値を取得する(dBではない)
+        /// </summary>
+        /// <param name="record">音データ</param>
+        /// <returns>FFT結果の絶対値</returns>
         private static List<double> ComputeFFT(List<float> record)
         {
             var window = Window.Hamming(samplePoint);
@@ -123,16 +187,10 @@ namespace sample
             List<double> magnitudes = new List<double>();
             foreach(System.Numerics.Complex comp in complexData)
             {
-                //magnitudes.Add(System.Math.Log(comp.Magnitude) * 20 + 100);
-                magnitudes.Add(comp.Magnitude * 20);
+                magnitudes.Add(comp.Magnitude * ampCoefficient);
             }
 
             return magnitudes;
-        }
-
-        private static void ShowLed(float sample)
-        {
-
         }
     }
 }
