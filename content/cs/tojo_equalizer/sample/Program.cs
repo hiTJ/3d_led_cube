ac45bb93e8855b331d1c2fca2404ae406527c31e
東條寛之 <hiroyuki.tojo@fxat.co.jp>
2017-08-04 14:47:13 +0900

    3D equalizer version.

--- content/cs/tojo_equalizer/sample/Program.cs
+++ content/cs/tojo_equalizer/sample/Program.cs
@@ -81,16 +81,18 @@ namespace sample
         {
             String test = AppDomain.CurrentDomain.BaseDirectory;
             DirectoryInfo libDir = Directory.GetParent(test).Parent.Parent.Parent.Parent;
-            SetDllDirectory(Path.Combine(libDir.FullName,"00_lib"));
+            SetDllDirectory(Path.Combine(libDir.FullName, "00_lib"));
             //SetDllDirectory("00_lib");
 
             using (var waveIn = new WaveInEvent())
             {
                 List<float> recorded = new List<float>();
                 waveIn.WaveFormat = new WaveFormat(sampleRate: sampleRate, channels: channels);
+                List<int[]> coordinates = new List<int[]>();
 
                 waveIn.DataAvailable += (_, e) =>
                 {
+                    int[] coordinate = new int[xAxisPoints];
                     // 32bitで最大値1.0fにする
                     for (int index = 0; index < e.BytesRecorded; index += 2)
                     {
@@ -111,56 +113,71 @@ namespace sample
                                 value += (int)(computed[i]);
                                 if (i % pointsPerBlock == 0 && i >= xAxisShifter * pointsPerBlock)
                                 {
-                                    for (int z = 0; z < zAxisPoints; z++)
+                                    if (value > pointsPerBlock * yAxisPoints)
                                     {
-                                        if (value > pointsPerBlock * yAxisPoints)
-                                        {
-                                            value = pointsPerBlock * yAxisPoints;
-                                        }
-                                        int highestY = yAxisPoints - value / pointsPerBlock;
-                                        for(int y = yAxisPoints; y > highestY; y--)
-                                        {
-                                            int color = 0;
-                                            if(y < 4)
-                                            {
-                                                color = 0xFF0000;
-                                            }
-                                            else if(4 <= y && y < 8)
-                                            {
-                                                color = 0xFF7700;
-                                            }
-                                            else if(8 <= y && y < 12)
-                                            {
-                                                color = 0x77FF00;
-                                            }
-                                            else if(16 <= y && y < 20)
-                                            {
-                                                color = 0x00FF00;
-                                            }
-                                            else if(20 <= y && y < 24)
-                                            {
-                                                color = 0x0077FF;
-                                            }
-                                            else if(24 <= y && y < 28)
-                                            {
-                                                color = 0x0000FF;
-                                            }
-                                            else
-                                            {
-                                                color = 0x000077;
-                                            }
-                                            SetLed(i / pointsPerBlock - xAxisShifter, y, z, color);
-                                        }
+                                        value = pointsPerBlock * yAxisPoints;
+                                    }
+                                    int highestY = yAxisPoints - value / pointsPerBlock;
+                                    //for (int y = yAxisPoints; y > highestY; y--)
+                                    {
+                                        //ここでLEDのパラメータをqueueに入れる
+                                        coordinate[i / pointsPerBlock - xAxisShifter] = highestY;
                                     }
                                     value = 0;
                                 }
                             }
-                            Show();
-                            Wait(10);
-                            Clear();
-                            recorded.Clear();
                         }
                     }
+                    coordinates.Add(coordinate);
+
+                    for (int z = 0; z < coordinates.Count; z++)
+                    {
+                        int color = 0;
+                        int[] xyCoordinate = coordinates[z];
+                        for (int x = 0; x < xAxisPoints; x++)
+                        {
+                            for (int y = yAxisPoints; y >= xyCoordinate[x]; y--)
+                            {
+                                if (y < 4)
+                                {
+                                    color = 0xFF0000;
+                                }
+                                else if (4 <= y && y < 8)
+                                {
+                                    color = 0xFF7700;
+                                }
+                                else if (8 <= y && y < 12)
+                                {
+                                    color = 0x77FF00;
+                                }
+                                else if (16 <= y && y < 20)
+                                {
+                                    color = 0x00FF00;
+                                }
+                                else if (20 <= y && y < 24)
+                                {
+                                    color = 0x0077FF;
+                                }
+                                else if (24 <= y && y < 28)
+                                {
+                                    color = 0x0000FF;
+                                }
+                                else
+                                {
+                                    color = 0x000077;
+                                }
+                                SetLed(x, y, zAxisPoints - z, color);
+                            }
+                        }
+                    }
+                    if (coordinates.Count > zAxisPoints)
+                    {
+                        coordinates.RemoveAt(0);
+                    }
+                    Show();
+                    Wait(10);
+                    Clear();
+                    recorded.Clear();
                 };
                 waveIn.StartRecording();
                 Console.WriteLine("Press ENTER to quit...");
